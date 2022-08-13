1f402810d2a7f8dc00a16650d43ac76b9363da6d
Arkadiusz Mika <arekmika4@gmail.com>
2022-08-13 16:18:56 +0200

    commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,120 +13,153 @@ class _LoginScreenState extends State<LoginScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color(0xFFFCFCFF),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          //   mainAxisSize: MainAxisSize.max,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 20, top: 50),
-              child: Text(
-                'Login',
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 10, left: 20),
-              child: Text(
-                'Please sign in to continue.',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  label: const Text('Email'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  label: const Text('Password'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-            const Align(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: EdgeInsets.only(right: 20, top: 10),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            //   mainAxisSize: MainAxisSize.max,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 20, top: 50),
                 child: Text(
-                  'Forget Password?',
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+                  'Login',
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF251F47)),
                 ),
               ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 40, right: 40, top: 30),
-                child: ElevatedButton(
-                  child: Text('Sign In'),
-                  onPressed: () {},
+              const Padding(
+                padding: EdgeInsets.only(top: 10, left: 20),
+                child: Text(
+                  'Please sign in to continue.',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
                 ),
               ),
-            ),
-            Center(
-                child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 60),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Container(
-                          height: 2,
-                          width: MediaQuery.of(context).size.width / 3,
-                          color: Colors.black,
-                        ),
-                      ),
-                      const Text(
-                        'or',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w400),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Container(
-                          height: 2,
-                          width: MediaQuery.of(context).size.width / 3,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    label: const Text('Email'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 30),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    label: const Text('Password'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              const Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 20, top: 10),
                   child: Text(
-                    'Login with',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    'Forget Password?',
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF251F47)),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      IconButton(onPressed: () {}, icon: Icon(Icons.facebook)),
-                      IconButton(onPressed: () {}, icon: Icon(Icons.email)),
-                    ],
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 40, top: 30),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xFF251F47),
+                      fixedSize: Size(MediaQuery.of(context).size.width, 45),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Text('Sign In'),
+                    onPressed: () {},
                   ),
                 ),
-              ],
-            ))
-          ],
+              ),
+              Center(
+                  child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 60),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Container(
+                            height: 2,
+                            width: MediaQuery.of(context).size.width / 3,
+                            color: const Color(0xFF251F47),
+                          ),
+                        ),
+                        const Text(
+                          'or',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w400),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Container(
+                            height: 2,
+                            width: MediaQuery.of(context).size.width / 3,
+                            color: const Color(0xFF251F47),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Text(
+                      'Login with',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF251F47)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.facebook,
+                              color: Color(0xFF251F47),
+                              size: 40,
+                            )),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.email,
+                              color: Color(0xFF251F47),
+                              size: 40,
+                            )),
+                      ],
+                    ),
+                  ),
+                ],
+              ))
+            ],
+          ),
         ),
       ),
     );
