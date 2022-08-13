dc62c895527913cd72552dad7db7adaf083560c7
Arkadiusz Mika <arekmika4@gmail.com>
2022-08-13 16:47:10 +0200

    commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,157 +8,190 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  final _form = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color(0xFFFCFCFF),
         body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            //   mainAxisSize: MainAxisSize.max,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(left: 20, top: 50),
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.w700,
-                      color: Color(0xFF251F47)),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 10, left: 20),
-                child: Text(
-                  'Please sign in to continue.',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
-                child: TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    label: const Text('Email'),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    label: const Text('Password'),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-              ),
-              const Align(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                  padding: EdgeInsets.only(right: 20, top: 10),
+          child: Form(
+            key: _form,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              //   mainAxisSize: MainAxisSize.max,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 20, top: 50),
                   child: Text(
-                    'Forget Password?',
+                    'Login',
                     style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
+                        fontSize: 50,
+                        fontWeight: FontWeight.w700,
                         color: Color(0xFF251F47)),
                   ),
                 ),
-              ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 40, right: 40, top: 30),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: const Color(0xFF251F47),
-                      fixedSize: Size(MediaQuery.of(context).size.width, 45),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    child: Text('Sign In'),
-                    onPressed: () {},
+                const Padding(
+                  padding: EdgeInsets.only(top: 10, left: 20),
+                  child: Text(
+                    'Please sign in to continue.',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
                   ),
                 ),
-              ),
-              Center(
-                  child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 60),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: Container(
-                            height: 2,
-                            width: MediaQuery.of(context).size.width / 3,
-                            color: const Color(0xFF251F47),
-                          ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
+                  child: TextFormField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      label: const Text('Email'),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Color(0xFF251F47),
+                          width: 2.0,
                         ),
-                        const Text(
-                          'or',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.w400),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'This field is required.';
+                      }
+                      if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      label: const Text('Password'),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Color(0xFF251F47),
+                          width: 2.0,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: Container(
-                            height: 2,
-                            width: MediaQuery.of(context).size.width / 3,
-                            color: const Color(0xFF251F47),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 30),
+                ),
+                const Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 20, top: 10),
                     child: Text(
-                      'Login with',
+                      'Forget Password?',
                       style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
                           color: Color(0xFF251F47)),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.facebook,
-                              color: Color(0xFF251F47),
-                              size: 40,
-                            )),
-                        const SizedBox(
-                          width: 10,
+                ),
+                Center(
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 30),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: const Color(0xFF251F47),
+                        fixedSize: Size(MediaQuery.of(context).size.width, 45),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
                         ),
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.email,
-                              color: Color(0xFF251F47),
-                              size: 40,
-                            )),
-                      ],
+                      ),
+                      child: Text('Sign In'),
+                      onPressed: () {},
                     ),
                   ),
-                ],
-              ))
-            ],
+                ),
+                Center(
+                    child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 60),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Container(
+                              height: 2,
+                              width: MediaQuery.of(context).size.width / 3,
+                              color: const Color(0xFF251F47),
+                            ),
+                          ),
+                          const Text(
+                            'or',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w400),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Container(
+                              height: 2,
+                              width: MediaQuery.of(context).size.width / 3,
+                              color: const Color(0xFF251F47),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: Text(
+                        'Login with',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF251F47)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.facebook,
+                                color: Color(0xFF251F47),
+                                size: 40,
+                              )),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.email,
+                                color: Color(0xFF251F47),
+                                size: 40,
+                              )),
+                        ],
+                      ),
+                    ),
+                  ],
+                ))
+              ],
+            ),
           ),
         ),
       ),
